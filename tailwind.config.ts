17c7756078e3ee175953471fbfbafcf34d268bc4
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-10-26 14:42:52 -0400

    feat: project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        body: "var(--font-geist-sans)",
+        mono: "var(--font-geist-mono)"
+      }
     },
   },
   plugins: [],
