eb84b1c3d1ef64bf7c0dcb67ff2e6de12223f220
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-10-28 00:15:06 -0400

    feat: partners section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./hero";
+import Partners from "./partners";
 
 export default function Home() {
   return (
-    <div className="min-h-screen max-w-[100vw] w-screen overflow-x-hidden relative">
+    <div className="min-h-screen max-w-[100vw] bg-[rgb(246,246,246)] w-screen overflow-x-hidden relative">
       <Hero />
+      <Partners />
     </div>
   );
 }
